6410c48c393457758883978e1fab96f4aa4f0f19
Simone Brundu <simone.brundu@gmail.com>
2016-06-02 20:07:07 +0200

    Sistemati marker e linee

--- js/maps.js
+++ js/maps.js
@@ -2,74 +2,107 @@ var map;
 //Angular App Module and Controller
 var sampleApp = angular.module('mapsApp', []);
 sampleApp.controller('controllerMarker', function ($scope, $http) {
-    $scope.formData = {};
+	$scope.formData = {};
 
-    // when landing on the page, get all markers and show them
-    $http.get('http://localhost:8083/getmarkers')
-        .success(function(data) {
-            initMap($scope, data);
-        })
-        .error(function(data) {
-            console.log('Error: ' + data);
-        });
-    
+// when landing on the page, get all markers and show them
+$http.get('http://localhost:8083/getmarkers')
+.success(function(data) {
+	initMap($scope, data);
+})
+.error(function(data) {
+	console.log('Error: ' + data);
+});
+
+$scope.onMarkerClicked = function(iataMarker){
+	removeAllLines($scope);
+	if($scope.lastIataMarkerClicked === iataMarker){
+		setMarkersOpacity($scope, 1);
+		$scope.lastIataMarkerClicked = null;
+	}else{
+		$http.get('http://localhost:8083/getroutesorigin/'+iataMarker)
+		.success(function(data) {		
+			for(var i = 0; i < data.length; i++){
+				drawLine($scope, data[i].OriginIata, data[i].DestIata);
+			}
+			setMarkersOpacity($scope, 0.6);
+			$scope.lastIataMarkerClicked = iataMarker;
+		})
+		.error(function(data) {
+			console.log('Error: ' + data);
+		});
+	}
+};
 });
 
 function initMap($scope, data) {
-    var mapOptions = {
-          zoom: 4,
-          center: new google.maps.LatLng(42,-83),
-          mapTypeId: google.maps.MapTypeId.TERRAIN
-      }
-      map = new google.maps.Map(document.getElementById("map"), mapOptions);
-      $scope.map = map;
-      $scope.markers = [];
+	var mapOptions = {
+		zoom: 4,
+		center: new google.maps.LatLng(37.4236178,-98.8819956),
+		mapTypeId: google.maps.MapTypeId.TERRAIN
+	}
+	map = new google.maps.Map(document.getElementById("map"), mapOptions);
+	$scope.map = map;
+	$scope.markers = [];
+	$scope.lines = [];
+	$scope.lastIataMarkerClicked;
 
-      var bounds = new google.maps.LatLngBounds();
-      var infoWindow = new google.maps.InfoWindow();
+	var bounds = new google.maps.LatLngBounds();
+	var infoWindow = new google.maps.InfoWindow();
 
-      var createMarker = function (info){
+	var createMarker = function (info){
+		var marker = new google.maps.Marker({
+			map: $scope.map,
+			position: new google.maps.LatLng(info.Latitude, info.Longitude),
+			title: info.LabelCity,
+			icon: 'js/icons/airport.png',
+			opacity: 1
+		});
 
-          var marker = new google.maps.Marker({
-              map: $scope.map,
-              position: new google.maps.LatLng(info.Latitude, info.Longitude),
-              title: info.LabelCity,
-              icon: 'js/icons/airport.png'
-          });
+		if(marker.title !== 'Pago Pago, TT' && marker.title !== 'Guam, TT')
+			bounds.extend(marker.position);
 
-          if(marker.title !== 'Pago Pago, TT' && marker.title !== 'Guam, TT')
-            bounds.extend(marker.position);
+		marker.content = '<div class="infoWindowContent">' + info.Iata + '</div>';
 
-          marker.content = '<div class="infoWindowContent">' + info.Iata + '</div>';
+		google.maps.event.addListener(marker, 'click', function(){
+			$scope.onMarkerClicked(info.Iata);
+		});
 
-          google.maps.event.addListener(marker, 'click', function(){
-              infoWindow.setContent('<h2>' + marker.title + '</h2>' + marker.content);
-              infoWindow.open($scope.map, marker);
-          });
+		$scope.markers[info.Iata] = marker;
+		map.fitBounds(bounds);
+	}  
 
-          $scope.markers.push(marker);
-          map.fitBounds(bounds);
-      }  
-
-      google.maps.event.addDomListener(window, "load", function() {
-      for (i = 0; i < data.length; i++){
-          createMarker(data[i]);
-        }
-      });
-        
-    google.maps.event.addDomListener(window, "load", function() {
-        drawLine($scope, data[0], data[1]);
-      }); 
+	google.maps.event.addDomListener(window, "load", function() {
+		for (i = 0; i < data.length; i++){
+			createMarker(data[i]);
+		}
+	});
 }
 
 function drawLine($scope, airport1, airport2) {
-    var flightPath = new google.maps.Polyline({
-        path: [$scope.markers[airport1].getPosition(), $scope.markers[airport2].getPosition()],
-        geodesic: true,
-        strokeColor: '#FF0000',
-        strokeOpacity: 1.0,
-        strokeWeight: 5
-    });
+	var flightPath = new google.maps.Polyline({
+		path: [$scope.markers[airport1].position, $scope.markers[airport2].position],
+		geodesic: true,
+		strokeColor: '#FF0000',
+		strokeOpacity: 1.0,
+		strokeWeight: 2
+	});
 
-    flightPath.setMap($scope.map);
+	$scope.lines.push(flightPath);
+
+	flightPath.setMap($scope.map);
+}
+
+function removeAllLines($scope){
+	if($scope.lines != null){
+		for (i=0; i<$scope.lines.length; i++) {                           
+			$scope.lines[i].setMap(null);
+		}
+		$scope.lines = [];
+	}
+}
+
+function setMarkersOpacity($scope, opacity){
+	for (var current in $scope.markers) {
+		$scope.markers[current].setOpacity(opacity);
+	}
 }
