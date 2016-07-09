6b20e461193fcfec6cf18fc3eea71068e759c70d
Aniello Guarino <guarinoaniello@hotmail.it>
2016-07-09 14:21:22 +0200

    .

--- server-side.js
+++ server-side.js
@@ -19,22 +19,8 @@ app.use(bodyParser.json());                                     // parse applica
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
 app.use(methodOverride());
 
-var ok = require('okay');
 app.use(require('express-domain-middleware'));
 app.use(app.router);
-app.use(function errorHandler(err, req, res, next) {
-  console.log('error on request %d %s %s: %j', process.domain.id, req.method, req.url, err);
-  res.send(500, "Something bad happened. :(");
-});
-app.get('/error', function(req, res, next) {
-  db.query('SELECT happiness()', ok(next, function(rows) {
-    fs.readFile('asldkfjasdf', ok(next, function(contents) {
-      process.nextTick(ok(next, function() {
-        throw new Error("The individual request will be passed to the express error handler, and your application will keep running.");
-      }));
-    }));
-  }));
-});
 
 // define models =================
 var allMarkers = mongoose.model('markers', {
